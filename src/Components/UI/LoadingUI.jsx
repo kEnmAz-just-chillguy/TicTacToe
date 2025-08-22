eea9b8584ff9da644f335a692d6b0cb998b3aad9
kEnmAz-just-chillguy <tolgaakif624@gmail.com>
2025-08-22 21:57:49 +0500

    New one player page

--- src/Components/UI/LoadingUI.jsx
+++ src/Components/UI/LoadingUI.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const LoadingUI = () => {
   return (
     <StyledWrapper>
-      <div className="loader ">
-        {Array.from({ length: 9 }).map((_, i) => (
-          <div key={i} className="square" />
-        ))}
+      <div className="loader">
+        <div className="cell d-0" />
+        <div className="cell d-1" />
+        <div className="cell d-2" />
+        <div className="cell d-1" />
+        <div className="cell d-2" />
+        <div className="cell d-2" />
+        <div className="cell d-3" />
+        <div className="cell d-3" />
+        <div className="cell d-4" />
       </div>
     </StyledWrapper>
   );
 };
 
-const pulse = keyframes`
-  0% { opacity: 0.4; }
-  50% { opacity: 1; }
-  100% { opacity: 0.4; }
-`;
-
 const StyledWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: fit-content; 
+  width: fit-content;
   height: fit-content;
 
   .loader {
@@ -32,31 +32,56 @@ const StyledWrapper = styled.div`
     gap: 0.8rem;
   }
 
-  .square {
+  .cell {
     width: 80px;
     height: 80px;
     border-radius: 0.75rem;
     border: 2px solid #000;
-    background: #2d3e4b;
-    animation: ${pulse} 1.2s ease-in-out infinite;
+    background-color: transparent;
+    animation: 1.5s ripple ease infinite;
   }
 
+  /* Задержки */
+  .cell.d-1 { animation-delay: 100ms; }
+  .cell.d-2 { animation-delay: 200ms; }
+  .cell.d-3 { animation-delay: 300ms; }
+  .cell.d-4 { animation-delay: 400ms; }
+
+  /* Цвета */
+  .cell:nth-child(1) { --cell-color: #00FF87; }
+  .cell:nth-child(2) { --cell-color: #0CFD95; }
+  .cell:nth-child(3) { --cell-color: #17FBA2; }
+  .cell:nth-child(4) { --cell-color: #23F9B2; }
+  .cell:nth-child(5) { --cell-color: #30F7C3; }
+  .cell:nth-child(6) { --cell-color: #3DF5D4; }
+  .cell:nth-child(7) { --cell-color: #45F4DE; }
+  .cell:nth-child(8) { --cell-color: #53F1F0; }
+  .cell:nth-child(9) { --cell-color: #60EFFF; }
+
+  @keyframes ripple {
+    0%   { background-color: transparent; }
+    30%  { background-color: var(--cell-color); }
+    60%  { background-color: transparent; }
+    100% { background-color: transparent; }
+  }
+
+  /* Адаптив */
   @media (min-width: 640px) {
-    .square {
+    .cell {
       width: 110px;
       height: 110px;
     }
   }
 
   @media (min-width: 768px) {
-    .square {
+    .cell {
       width: 140px;
       height: 140px;
     }
   }
 
   @media (min-width: 1024px) {
-    .square {
+    .cell {
       width: 170px;
       height: 170px;
     }
--- src/pages/OnePlayerPage.jsx
+++ src/pages/OnePlayerPage.jsx
@@ -1,57 +1,208 @@
 import React from 'react'
+import { useState } from 'react'
+import circle_icon from "/circle.png"
+import cross_icon from "/cross.png"
 
 function OnePlayerPage() {
+  const [level, setLevel] = useState("");
+  const [board,setBoard] = useState(Array(9).fill(""));
+  const [isTurn, setIsTurn] = useState(true)
+
+  const toggle = (index) => {
+    if (board[index] !== "" || !isTurn) return;
+
+    setBoard((prev) => {
+      let newBoard = [...prev]
+       newBoard[index] = "x"
+       return newBoard
+    } )
+
+    const winner = checkWinner(newBoard);
+    if (winner) {
+      alert(winner.toUpperCase() + " wins!");
+      return newBoard;
+    }
+
+    setIsTurn(false)
+    setTimeout( botMove , 500);
+  }
+  const botMove = () => {
+    setBoard((prev) => {
+      let emptyIndexes = prev
+        .map((val, i) => (val === "" ? i : null))
+        .filter((v) => v !== null);
+  
+      if (emptyIndexes.length === 0) return prev;
+  
+      let randomIndex =
+        emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
+  
+      let newBoard = [...prev];
+      newBoard[randomIndex] = "o";
+  
+      
+      const winner = checkWinner(newBoard);
+      if (winner) {
+        console.log("Winner is:", winner);
+      }
+  
+      
+      return newBoard;
+    });
+  
+    setTimeout(() => setIsTurn(true), 50);
+  };
+  
+  
+  const checkWinner = (board) => {
+    const winLines = [
+      [0,1,2]
+      [3,4,5]
+      [6,7,8]
+      [0,3,6]
+      [1,4,7]
+      [2,5,8]
+      [0,4,8]
+      [2,4,6]
+    ]
+    for ( let [a,b,c] of winLines) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]){
+        return board[a]
+      }
+    }
+    return null
+  }
+  
   return (
  <>
- <div className="select-none">
-  <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
-    <div className="flex flex-col sm:flex-row gap-6 sm:gap-16 items-center justify-center">
+{
+  !level ? 
+  (
+    <div className="select-none">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
+      <div className="flex flex-col sm:flex-row gap-6 sm:gap-16 items-center justify-center">
+        
       
-      {/* One Player */}
-      <div className="bg-green-600 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 rounded-xl  text-center border-2 sm:border-4 border-yellow-400 shadow-[0_0_10px_rgba(255,255,0,0.8)] hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,0,1)] transition-all duration-300">
-        <button to="/one-player" className="text-center group">
-          <div className="flex flex-col items-center  gap-3">
+        <div className="bg-green-600 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 rounded-xl  text-center border-2 sm:border-4 border-yellow-400 shadow-[0_0_10px_rgba(255,255,0,0.8)] hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,0,1)] transition-all duration-300">
+          <button onClick={() => setLevel("easy")} className="text-center group">
+            <div className="flex flex-col items-center  gap-3">
+              <div className="mt-3 w-14 sm:w-20 md:w-24 text-5xl sm:text-6xl md:text-7xl transition-transform duration-300 group-hover:scale-110">
+                🤓
+              </div>
+              <p className="font-bold text-lg sm:text-2xl md:text-3xl text-yellow-300 drop-shadow-[1px_1px_2px_rgba(0,0,0,0.8)] select-none">
+            Easy
+          </p>
+            </div>
+          </button>
+        </div>
+  
+        <div className="bg-blue-600 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 rounded-xl  text-center border-2 sm:border-4 border-yellow-400 shadow-[0_0_10px_rgba(255,255,0,0.8)] hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,0,1)] transition-all duration-300">
+          <button onClick={() => setLevel("normal")} className="text-center group">
+            <div className="flex flex-col items-center  gap-3">
+              <div className="mt-3 w-14 sm:w-20 md:w-24 text-5xl sm:text-6xl md:text-7xl transition-transform duration-300 group-hover:scale-110">
+                😎
+              </div>
+              <p className="font-bold text-lg sm:text-2xl md:text-3xl text-yellow-300 drop-shadow-[1px_1px_2px_rgba(0,0,0,0.8)] select-none">
+            Normal
+          </p>
+            </div>
+          </button>
+        </div>
+  
+        <div className="bg-red-600 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 rounded-xl text-center border-2 sm:border-4 border-yellow-400 shadow-[0_0_10px_rgba(255,255,0,0.8)] hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,0,1)] transition-all duration-300">
+          <button onClick={() => setLevel("hard")}  className="group">
+            <div className="flex flex-col items-center gap-3">
             <div className="mt-3 w-14 sm:w-20 md:w-24 text-5xl sm:text-6xl md:text-7xl transition-transform duration-300 group-hover:scale-110">
-              🤓
-            </div>
+            🧠
+              </div>
             <p className="font-bold text-lg sm:text-2xl md:text-3xl text-yellow-300 drop-shadow-[1px_1px_2px_rgba(0,0,0,0.8)] select-none">
-          Easy
-        </p>
-          </div>
-        </button>
-      </div>
-
-      <div className="bg-blue-600 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 rounded-xl  text-center border-2 sm:border-4 border-yellow-400 shadow-[0_0_10px_rgba(255,255,0,0.8)] hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,0,1)] transition-all duration-300">
-        <button to="/one-player" className="text-center group">
-          <div className="flex flex-col items-center  gap-3">
-            <div className="mt-3 w-14 sm:w-20 md:w-24 text-5xl sm:text-6xl md:text-7xl transition-transform duration-300 group-hover:scale-110">
-              😎
+            Hard
+          </p>
             </div>
-            <p className="font-bold text-lg sm:text-2xl md:text-3xl text-yellow-300 drop-shadow-[1px_1px_2px_rgba(0,0,0,0.8)] select-none">
-          Normal
-        </p>
-          </div>
-        </button>
+          </button>
+   
+        </div>
+  
       </div>
-
-      {/* Two Players */}
-      <div className="bg-red-600 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 rounded-xl text-center border-2 sm:border-4 border-yellow-400 shadow-[0_0_10px_rgba(255,255,0,0.8)] hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,0,1)] transition-all duration-300">
-        <button to="/two-players" className="group">
-          <div className="flex flex-col items-center gap-3">
-          <div className="mt-3 w-14 sm:w-20 md:w-24 text-5xl sm:text-6xl md:text-7xl transition-transform duration-300 group-hover:scale-110">
-          🧠
-            </div>
-          <p className="font-bold text-lg sm:text-2xl md:text-3xl text-yellow-300 drop-shadow-[1px_1px_2px_rgba(0,0,0,0.8)] select-none">
-          Hard
-        </p>
-          </div>
-        </button>
- 
-      </div>
-
     </div>
   </div>
-</div>
+  ) : 
+  (
+    
+  <div className="flex flex-col items-center px-4 w-full">
+
+  <div className="flex flex-col items-center gap-5 mt-6">
+    <p className="text-white font-bold text-xl sm:text-2xl md:text-3xl text-center min-h-[2.5rem] sm:min-h-[3rem]">
+      AI has won the game
+    </p>
+  </div>
+
+ 
+
+ {/* {loading  */}
+  {/* ?  */}
+  {/* <div className='flex justify-center mt-6 mb-6 w-full'>    <LoadingUI/> </div> */}
+
+         
+   <div className="flex justify-center mt-6 mb-6 w-full">
+    <div className="grid grid-cols-3 gap-2 sm:gap-3">
+      {board.map((value, index) => (
+        <div
+          key={index}
+          onClick={() => toggle(index)}
+          className="select-none rounded-xl border-2 border-black bg-[#1f3540] 
+                     w-[80px] h-[80px] sm:w-[110px] sm:h-[110px] md:w-[140px] md:h-[140px] lg:w-[170px] lg:h-[170px] 
+                     flex items-center justify-center"
+        >
+          {value === "x" && (
+            <img
+              src={cross_icon}
+              className="w-[40px] sm:w-[55px] md:w-[75px] lg:w-[95px]"
+            />
+          )}
+          {value === "o" && (
+            <img
+              src={circle_icon}
+              className="w-[40px] sm:w-[55px] md:w-[75px] lg:w-[95px]"
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  </div>
+
+
+   {/* Buttons */}
+{/* //   <div className="flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-10 w-full max-w-[600px] px-4"> */}
+//     {/* Go Back */}
+{/* //     <button */}
+{/* //       onClick={leavePage} */}
+{/* //       className="bg-[hsl(49,98%,60%)] text-center w-40 sm:w-48 h-12 sm:h-14 mt-2 sm:mt-5  */}
+{/* //                  rounded-2xl relative text-white text-lg sm:text-xl font-semibold group overflow-hidden" */}
+{/* //       type="button" */}
+{/* //     > */}
+{/* //       <div className="bg-red-600 rounded-xl h-10 sm:h-12 w-1/4 flex items-center justify-center  */}
+{/* //                       absolute left-1 top-[4px] group-hover:w-[90%] z-10 duration-500"> */}
+{/* //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="22px" width="22px"> */}
+{/* //           <path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="#000000" /> */}
+{/* //           <path d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" fill="#000000" /> */}
+{/* //         </svg> */}
+{/* //       </div> */}
+{/* //       <p */}
+{/* //         className="translate-x-2 font-semibold text-lg sm:text-xl" */}
+{/* //         style={{ textShadow: "2px 2px rgb(116,116,116)" }} */}
+{/* //       > */}
+{/* //         Go Back */}
+{/* //       </p> */}
+{/* //     </button> */}
+
+//     {/* Reset */}
+{/* //     <ButtonUI onCustomClick={resetGame} /> */}
+{/* //   </div> */}
+// </div> 
+
+  )
+}
 
  </>
   )
